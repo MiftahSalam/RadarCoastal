0e7161417c5f30651a6da954f4151e7c8093f7ea
Miftah Salam <salam.miftah@gmail.com>
2024-01-26 13:51:35 +0700

    refactor arpa sender message decoder

--- RadarCoastal/usecase/stream/arpasender.h
+++ RadarCoastal/usecase/stream/arpasender.h
@@ -9,14 +9,80 @@
 #include "infra/stream/stream.h"
 #include <RadarEngine/radarconfig.h>
 
+struct ArpaSenderModel
+{
+    int id;
+    double lat;
+    double lon;
+    double alt;
+    double rng;
+    double brn;
+    double spd;
+    double crs;
+};
+
+class ArpaSenderDecoder
+{
+public:
+    ArpaSenderDecoder(int id,
+                      double lat,
+                      double lon,
+                      double alt,
+                      double rng,
+                      double brn,
+                      double spd,
+                      double crs);
+    ArpaSenderDecoder(TrackModel data);
+
+    virtual ~ArpaSenderDecoder(){}
+
+    virtual QString decode() = 0;
+
+protected:
+    ArpaSenderModel m_data;
+};
+
+class ArpaSenderDecoderJson: public ArpaSenderDecoder
+{
+public:
+    ArpaSenderDecoderJson(int id,
+                          double lat,
+                          double lon,
+                          double alt,
+                          double rng,
+                          double brn,
+                          double spd,
+                          double crs);
+    ArpaSenderDecoderJson(TrackModel data);
+
+    // ArpaSenderDecoder interface
+    QString decode() override;
+};
+
+class ArpaSenderDecoderNMEA: public ArpaSenderDecoder
+{
+public:
+    ArpaSenderDecoderNMEA(int id,
+                          double lat,
+                          double lon,
+                          double alt,
+                          double rng,
+                          double brn,
+                          double spd,
+                          double crs);
+    ArpaSenderDecoderNMEA(TrackModel data);
+
+    // ArpaSenderDecoder interface
+    QString decode() override;
+};
+
 class ArpaSender : public QObject
 {
     Q_OBJECT
 public:
     explicit ArpaSender(QObject *parent = nullptr);
 
-    void SendData(const QString data);
-    void SendData(const TrackModel& data);
+    void SendData(TrackModel data);
     void SendData(
             int id,
             double lat,
